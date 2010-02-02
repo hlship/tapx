63d35e29afbf10b5c4e6f3168237dc86a72f4bd1
Howard M. Lewis Ship <hlship@gmail.com>
2010-02-02 10:53:43 -0800

    Add temporary workaround for overly zealous Tapestry 5.2 security restrictions

--- tapx-datefield/src/main/java/com/howardlewisship/tapx/datefield/services/DateFieldModule.java
+++ tapx-datefield/src/main/java/com/howardlewisship/tapx/datefield/services/DateFieldModule.java
@@ -1,10 +1,10 @@
-// Copyright 2009 Howard M. Lewis Ship
+// Copyright 2009, 2010 Howard M. Lewis Ship
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -34,7 +34,7 @@ public class DateFieldModule
     public static void contributeFactoryDefaults(MappedConfiguration<String, String> configuration)
     {
         configuration.add(DateFieldSymbols.JSCALENDAR_PATH,
-                          "classpath:/com/howardlewisship/tapx/datefield/jscalendar-1.0");
+                "classpath:/com/howardlewisship/tapx/datefield/jscalendar-1.0");
         configuration.add(DateFieldSymbols.SKIN, "aqua");
         configuration.add(DateFieldSymbols.THEME, "system");
     }
@@ -44,16 +44,29 @@ public class DateFieldModule
         configuration.add(new LibraryMapping("tapx", "com.howardlewisship.tapx.datefield"));
     }
 
-    public static void contributeClasspathAssetAliasManager(MappedConfiguration<String, String> configuration)
+    public static void contributeClasspathAssetAliasManager(
+            MappedConfiguration<String, String> configuration)
     {
-        String version = VersionUtils.readVersionNumber(
-                "META-INF/maven/com.howardlewisship/tapx-datefield/pom.properties");
+        String version = VersionUtils
+                .readVersionNumber("META-INF/maven/com.howardlewisship/tapx-datefield/pom.properties");
 
-        configuration.add(String.format("tapx/%s/datefield", version), "com/howardlewisship/tapx/datefield");
+        configuration.add(String.format("tapx/%s/datefield", version),
+                "com/howardlewisship/tapx/datefield");
     }
 
-    public static void contributeBeanBlockOverrideSource(Configuration<BeanBlockContribution> configuration)
+    public static void contributeBeanBlockOverrideSource(
+            Configuration<BeanBlockContribution> configuration)
     {
-        configuration.add(new BeanBlockContribution("date", "tapx/DateFieldEditBlocks", "date", true));
+        configuration.add(new BeanBlockContribution("date", "tapx/DateFieldEditBlocks", "date",
+                true));
+    }
+
+    /**
+     * Temporary: enable access to all of tapx as T5.2.0 is overly protective.
+     */
+    public void contributeRegexAuthorizer(
+            org.apache.tapestry5.ioc.Configuration<String> configuration)
+    {
+        configuration.add("^com/howardlewisship/tapx/.*");
     }
 }
