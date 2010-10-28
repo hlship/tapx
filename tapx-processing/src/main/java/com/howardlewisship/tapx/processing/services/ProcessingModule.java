8fba1b7ee9a1c65f32b066770593f8b82596eb3a
Howard M. Lewis Ship <hlship@gmail.com>
2010-10-28 09:48:11 -0700

    Add new tapx-processing module

---
+++ tapx-processing/src/main/java/com/howardlewisship/tapx/processing/services/ProcessingModule.java
@@ -0,0 +1,29 @@
+// Copyright 2010 [ORG]
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.howardlewisship.tapx.processing.services;
+
+import org.apache.tapestry5.ioc.Configuration;
+import org.apache.tapestry5.ioc.annotations.Contribute;
+import org.apache.tapestry5.services.ComponentClassResolver;
+import org.apache.tapestry5.services.LibraryMapping;
+
+public class ProcessingModule
+{
+    @Contribute(ComponentClassResolver.class)
+    public static void extendTapxLibraryMapping(Configuration<LibraryMapping> configuration)
+    {
+        configuration.add(new LibraryMapping("tapx", "com.howardlewisship.tapx.processing"));
+    }
+}
