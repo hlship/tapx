0b086e0a0aa2782906c20e21493b857bf32c6dd2
Jochen Berger <fooberger@googlemail.com>
2010-12-02 10:08:55 +0100

    bean_editor_override test fails if run in a browser that will prefer de or fr locales over en

--- tapx-datefield/src/test/java/demo/pages/BeanEditorDemo.java
+++ tapx-datefield/src/test/java/demo/pages/BeanEditorDemo.java
@@ -16,14 +16,32 @@ package demo.pages;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import demo.data.NameAndDate;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.internal.TapestryInternalUtils;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.PersistentLocale;
 
 public class BeanEditorDemo
 {
     @Property
     private NameAndDate bean;
+
+    @Inject
+    private PersistentLocale persistentLocale;
+
+    Object onActivate() {
+        // make sure we serve the page with an English locale as the test case
+        // will fail otherwise
+        if (!TapestryInternalUtils.isEqual(persistentLocale.get(), Locale.ENGLISH))
+        {
+            persistentLocale.set(Locale.ENGLISH);
+            return BeanEditorDemo.class;
+        }
+        return null;
+    }
 
     void setupRender()
     {
