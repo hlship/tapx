091f096cd30569771ae49bc6d1b989c23a216abd
Howard M. Lewis Ship <hlship@gmail.com>
2010-12-07 11:29:26 -0800

    Extend the test layout to include a link to reset the session

--- tapx-datefield/src/test/java/demo/components/Layout.java
+++ tapx-datefield/src/test/java/demo/components/Layout.java
@@ -1,6 +1,19 @@
 package demo.components;
 
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.Session;
+
 public class Layout
 {
+    @Inject
+    private Request request;
 
+    void onReset()
+    {
+        Session session = request.getSession(false);
+
+        if (session != null)
+            session.invalidate();
+    }
 }
