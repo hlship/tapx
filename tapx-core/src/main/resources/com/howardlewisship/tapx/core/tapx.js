f268b62b40b163a43f3db4c93e92888cca597ed1
Howard M. Lewis Ship <hlship@gmail.com>
2011-03-15 17:09:33 -0700

    Rebuild the TapxCore JavaScript using a modular style

--- tapx-core/src/main/resources/com/howardlewisship/tapx/core/tapx.js
+++ tapx-core/src/main/resources/com/howardlewisship/tapx/core/tapx.js
@@ -1,31 +1,22 @@
 Tapx = {
-	SetEditor : {
 
-		updateHiddenField : function(field, state) {
-			field.value = state.values.toJSON();
-		},
+	/**
+	 * Extends an object using a source. In the simple case, the source object's
+	 * properties are overlayed on top of the destination object. In the typical
+	 * case, the source parameter is a function that returns the source object
+	 * ... this is to faciliate modularity and encapsulation.
+	 * 
+	 * @param destination
+	 *            object to receive new or updated properties
+	 * @param source
+	 *            source object for properties, or function returning source
+	 *            object
+	 */
+	extend : function(destination, source) {
+		if (Object.isFunction(source))
+			source = source();
 
-		addItemToList : function(value, state, listElement, hiddenFieldElement) {
-			var listItemElement = new Element("li")
-					.update(Object.toHTML(value));
-
-			var deleteElement = new Element("span", {
-				"class" : "tx-seteditor-delete",
-				title : "Delete"
-			});
-
-			deleteElement.observe("click", function(event) {
-				event.stop();
-
-				state.values = state.values.without(value);
-				hiddenFieldElement.value = state.values.toJSON();
-
-				listItemElement.remove();
-			});
-
-			listItemElement.insert(deleteElement);
-			listElement.insert(listItemElement);
-		}
+		Object.extend(destination, source);
 	},
 
 	Tree : {
@@ -33,169 +24,213 @@ Tapx = {
 		/** Approximate time per pixel for the hide and reveal animations. */
 		ANIMATION_RATE : .005,
 
+		/** Maximum animation time, in seconds. */
 		MAX_ANIMATION_DURATION : .5,
 
+		/** Type of Scriptaculous effect to hide/show child nodes. */
 		TOGGLE_TYPE : 'blind',
 
-		QUEUE_NAME : 'tx-tree-updates',
-
-		doAnimate : function(element) {
-			var sublist = $(element).up('li').down("ul");
-
-			var dim = sublist.getDimensions();
-
-			var duration = Math.min(dim.height * this.ANIMATION_RATE,
-					this.MAX_ANIMATION_DURATION)
-
-			new Effect.toggle(sublist, this.TOGGLE_TYPE, {
-				duration : duration,
-				queue : {
-					position : 'end',
-					scope : this.QUEUE_NAME
-				}
-			});
-		},
-
-		animateRevealChildren : function(element) {
-			$(element).addClassName("tx-tree-expanded");
-
-			this.doAnimate(element);
-		},
-
-		animateHideChildren : function(element) {
-			$(element).removeClassName("tx-tree-expanded");
-
-			this.doAnimate(element);
-		}
+		/**
+		 * Name of Scripactulous effects queue to ensure that animations do not
+		 * overwrite each other.
+		 */
+		QUEUE_NAME : 'tx-tree-updates'
 	}
 };
 
-Tapestry.Initializer.tapxSetEditor = function(spec) {
+Tapx.extend(Tapestry.Initializer, function() {
 
-	/**
-	 * A shared object that is mutable; adding or removing a value updates
-	 * state.values
-	 */
-	var state = {
-		values : $A(spec.values)
-	};
+	function updateHiddenField(field, state) {
+		field.value = state.values.toJSON();
+	}
 
-	var textField = $(spec.id);
+	function addItemToList(value, state, listElement, hiddenFieldElement) {
+		var listItemElement = new Element("li").update(Object.toHTML(value));
 
-	var hiddenField = new Element("input", {
-		type : "hidden",
-		name : spec.name,
-		value : state.values.toJSON()
-	});
-
-	textField.insert({
-		after : hiddenField
-	});
-
-	var valuesColumn = textField.up(".tx-seteditor").down(".tx-valuescolumn");
-
-	var valueList = new Element("ul");
-
-	valuesColumn.insert(valueList);
-
-	state.values.each(function(value) {
-		Tapx.SetEditor.addItemToList(value, state, valueList, hiddenField);
-	});
-
-	textField.observe("keypress", function(event) {
-		if (event.keyCode != Event.KEY_RETURN)
-			return;
-
-		event.stop();
-
-		var value = textField.value;
-
-		if (value == null || value.empty())
-			return;
-
-		Tapx.SetEditor.addItemToList(value, state, valueList, hiddenField);
-
-		state.values.push(value);
-
-		Tapx.SetEditor.updateHiddenField(hiddenField, state);
-
-		textField.value = "";
-	});
-};
-
-Tapestry.Initializer.tapxTreeNode = function(spec) {
-
-	var loaded = spec.expanded;
-	var expanded = spec.expanded;
-	var loading = false;
-
-	function successHandler(reply) {
-		// Remove the Ajax load indicator
-		$(spec.clientId).update("");
-		$(spec.clientId).removeClassName("tx-empty-node");
-
-		var response = reply.responseJSON;
-
-		Tapestry.loadScriptsInReply(response, function() {
-			var element = $(spec.clientId).up("li");
-			var label = element.down("span.tx-tree-label");
-
-			label.insert({
-				after : response.content
-			});
-
-			// Hide the new sublist so that we can animate revealing it.
-			element.down("ul").hide();
-
-			Tapx.Tree.animateRevealChildren(spec.clientId);
-
-			loading = false;
-			loaded = true;
-			expanded = true;
+		var deleteElement = new Element("span", {
+			"class" : "tx-seteditor-delete",
+			title : "Delete"
 		});
 
+		deleteElement.observe("click", function(event) {
+			event.stop();
+
+			state.values = state.values.without(value);
+			hiddenFieldElement.value = state.values.toJSON();
+
+			listItemElement.remove();
+		});
+
+		listItemElement.insert(deleteElement);
+		listElement.insert(listItemElement);
 	}
 
-	function doLoad() {
-		if (loading)
-			return;
+	function initializer(spec) {
 
-		loading = true;
+		/**
+		 * A shared object that is mutable; adding or removing a value updates
+		 * state.values
+		 */
+		var state = {
+			values : $A(spec.values)
+		};
 
-		$(spec.clientId).addClassName("tx-empty-node");
-		$(spec.clientId).update("<span class='tx-ajax-wait'/>");
+		var textField = $(spec.id);
 
-		Tapestry.ajaxRequest(spec.expandChildrenURL, successHandler);
+		var hiddenField = new Element("input", {
+			type : "hidden",
+			name : spec.name,
+			value : state.values.toJSON()
+		});
+
+		textField.insert({
+			after : hiddenField
+		});
+
+		var valuesColumn = textField.up(".tx-seteditor").down(
+				".tx-valuescolumn");
+
+		var valueList = new Element("ul");
+
+		valuesColumn.insert(valueList);
+
+		state.values.each(function(value) {
+			addItemToList(value, state, valueList, hiddenField);
+		});
+
+		textField.observe("keypress", function(event) {
+			if (event.keyCode != Event.KEY_RETURN)
+				return;
+
+			event.stop();
+
+			var value = textField.value;
+
+			if (value == null || value.empty())
+				return;
+
+			addItemToList(value, state, valueList, hiddenField);
+
+			state.values.push(value);
+
+			updateHiddenField(hiddenField, state);
+
+			textField.value = "";
+		});
 	}
 
-	$(spec.clientId).observe(
-			"click",
-			function(event) {
-				event.stop();
+	return {
+		tapxSetEditor : initializer
+	};
+});
 
-				if (!loaded) {
+Tapx.extend(Tapestry.Initializer, function() {
 
-					doLoad();
+	var cfg = Tapx.Tree;
 
-					return;
-				}
+	function doAnimate(element) {
+		var sublist = $(element).up('li').down("ul");
 
-				// Children have been loaded, just a matter of toggling between
-				// showing or hiding the children.
+		var dim = sublist.getDimensions();
 
-				var f = expanded ? Tapx.Tree.animateHideChildren
-						: Tapx.Tree.animateRevealChildren;
+		var duration = Math.min(dim.height * cfg.ANIMATION_RATE,
+				cfg.MAX_ANIMATION_DURATION)
 
-				f.call(Tapx.Tree, spec.clientId);
+		new Effect.toggle(sublist, cfg.TOGGLE_TYPE, {
+			duration : duration,
+			queue : {
+				position : 'end',
+				scope : cfg.QUEUE_NAME
+			}
+		});
+	}
 
-				var url = expanded ? spec.markCollapsedURL
-						: spec.markExpandedURL;
+	function animateRevealChildren(element) {
+		$(element).addClassName("tx-tree-expanded");
 
-				Tapestry.ajaxRequest(url, {});
+		doAnimate(element);
+	}
 
-				expanded = !expanded;
+	function animateHideChildren(element) {
+		$(element).removeClassName("tx-tree-expanded");
+
+		doAnimate(element);
+	}
+
+	function initializer(spec) {
+		var loaded = spec.expanded;
+		var expanded = spec.expanded;
+		var loading = false;
+
+		function successHandler(reply) {
+			// Remove the Ajax load indicator
+			$(spec.clientId).update("");
+			$(spec.clientId).removeClassName("tx-empty-node");
+
+			var response = reply.responseJSON;
+
+			Tapestry.loadScriptsInReply(response, function() {
+				var element = $(spec.clientId).up("li");
+				var label = element.down("span.tx-tree-label");
+
+				label.insert({
+					after : response.content
+				});
+
+				// Hide the new sublist so that we can animate revealing it.
+				element.down("ul").hide();
+
+				animateRevealChildren(spec.clientId);
+
+				loading = false;
+				loaded = true;
+				expanded = true;
 			});
-}
+
+		}
+
+		function doLoad() {
+			if (loading)
+				return;
+
+			loading = true;
+
+			$(spec.clientId).addClassName("tx-empty-node");
+			$(spec.clientId).update("<span class='tx-ajax-wait'/>");
+
+			Tapestry.ajaxRequest(spec.expandChildrenURL, successHandler);
+		}
+
+		$(spec.clientId).observe("click", function(event) {
+			event.stop();
+
+			if (!loaded) {
+
+				doLoad();
+
+				return;
+			}
+
+			// Children have been loaded, just a matter of toggling
+			// between
+			// showing or hiding the children.
+
+			var f = expanded ? animateHideChildren : animateRevealChildren;
+
+			f.call(null, spec.clientId);
+
+			var url = expanded ? spec.markCollapsedURL : spec.markExpandedURL;
+
+			Tapestry.ajaxRequest(url, {});
+
+			expanded = !expanded;
+		});
+	}
+
+	return {
+		tapxTreeNode : initializer
+	};
+});
 
 Tapestry.Initializer.tapxExpando = function(spec) {
 
